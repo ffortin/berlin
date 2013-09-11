b7ebe74c71b0133eb30e6c7a9c188bd7c24f1fde
Jeremie Lasalle Ratelle <jeremie.ratelle@gmail.com>
2013-09-10 21:08:48 -0400

    lalal

--- test.rb
+++ test.rb
@@ -1,24 +1,19 @@
 require 'berlin-ai'         # Require the berlin-ai library.
 
-  def compute_score(node)
-    node.soldiers_per_turn + node.points
-  end
+def compute_score(node)
+  node.soldiers_per_turn + node.points
+end
 
-  def better_node(node1, node2)
-    return node1 if node2 == nil
-    return node2 if node1 == nil
-    compute_score(node1) > compute_score(node2) ? node1 : node2
-  end
+def better_node(node1, node2)
+  return node1 if node2.nil?
+  return node2 if node1.nil?
+  compute_score(node1) > compute_score(node2) ? node1 : node2
+end
 
 
 class Berlin::AI::Player
 
-
-
-  def self.on_turn(game)         # Implement the on_turn method of Berlin::AI::Player.
-    # Do your magic here.
-
-    # Here's an AI that randomly moves soldiers from node to node.
+  def self.on_turn(game)
     game.map.controlled_nodes.each do |node|
 
       soldiers = node.number_of_soldiers
@@ -27,7 +22,7 @@ class Berlin::AI::Player
       soldiers_to_send = soldiers - soldiers_to_keep
 
       winner_node = nil
-      
+
       if soldiers_to_send > 0
         node.adjacent_nodes.shuffle.each do |other_node|
           winner_node = better_node(other_node, winner_node)
